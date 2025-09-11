4f33cb9b40d6cf5b80594d3b8c504ebfacd81f81
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2025-09-11 14:30:08 +0200

    Change zip file creation to tar.xz format

--- release_zips/release_tar.py
+++ release_zips/release_tar.py
@@ -26,7 +26,7 @@ def main():
 
     if repo.private:
         # For private repos, git needs authentication (but set so that the
-        # remote URL doesn't embed the temporary credentials in the zip file or
+        # remote URL doesn't embed the temporary credentials in the tar.xz file or
         # even store the temporary credential token in the filesystem.)
         subprocess.run(["git",  "config", "--global", "credential.https://github.com.username", github_actor], check=True)
         helper_cmd  = "!f() { test \"$1\" = get && echo \"password=$GITHUB_TOKEN\"; }; f"  # shell command
@@ -41,7 +41,6 @@ def main():
     subprocess.run(["git", "clone", "--recursive", "--branch", tag, git_url, directory], check=True)
 
     # remove added extra components
-    shutil.rmtree(directory + "/components/esp_dsp", ignore_errors=True)
     shutil.rmtree(directory + "/components/esp32-camera", ignore_errors=True)
     shutil.rmtree(directory + "/components/esp_littlefs", ignore_errors=True)
     # remove docs
@@ -49,16 +48,18 @@ def main():
     # remove examples; enabling will brake Platformio Platform CI
     #shutil.rmtree(directory + "/examples", ignore_errors=True)
 
-    zipfile = "{}.zip".format(directory)
+    tarfile = "{}.tar.xz".format(directory)
 
-    print("Zip needed files into {}...".format(zipfile))
-    subprocess.run(["/usr/bin/7z", "a", "-mx=9", "-tzip", "-xr!.*", zipfile, directory], check=True)
+    print("Creating tar.xz archive {}...".format(tarfile))
+    # Create tar.xz with maximum compression
+    # --exclude-vcs-ignores excludes files matching .gitignore patterns
+    subprocess.run(["tar", "-cJf", "--exclude-vcs-ignores", "--exclude=.*", tarfile, directory], check=True, env={**os.environ, "XZ_OPT": "-9"})
 
     try:
         release = repo.get_release(tag)
         print("Existing release found...")
-        if any(asset.name == zipfile for asset in release.get_assets()):
-            raise SystemExit("A release for tag {} already exists and has a zip file {}. Workflow configured wrong?".format(tag, zipfile))
+        if any(asset.name == tarfile for asset in release.get_assets()):
+            raise SystemExit("A release for tag {} already exists and has a tar.xz file {}. Workflow configured wrong?".format(tag, tarfile))
     except GithubException:
         print("Creating release...")
         is_prerelease = "-" in tag  # tags like vX.Y-something are pre-releases
@@ -70,11 +71,11 @@ def main():
                                           "(Draft created by Action)",
                                           draft=True, prerelease=is_prerelease)
 
-    print("Attaching zipfile(s)...")
-    release.upload_asset(zipfile)
+    print("Attaching tar.xz file...")
+    release.upload_asset(tarfile)
 
-    print("Release URL is {}".format(release.html_url)
-)
+    print("Release URL is {}".format(release.html_url))
+
 
 if __name__ == "__main__":
     main()
